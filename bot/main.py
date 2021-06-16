3161f7e0620718d30bd96f1c2fb161a5031ccb9b
Ganji <avsv3@ya.ru>
2021-06-16 13:52:29 +0300

    add !fox command

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,9 @@
 import os
-from discord.ext import commands
 
+import discord
+from discord.ext import commands
+import json
+import requests
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -11,6 +14,14 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("Привет медвед!")
+
+@bot.command()
+async def fox(ctx):
+    response = requests.get('https://some-random-api.ml/img/fox')
+    json_data = json.loads(response.text)
+    embed = discord.Embed(color=0xff9900, title='Рандомная лисичка')
+    embed.set_image(url=json_data['link'])
+    await  ctx.send(embed=embed)
 
 if __name__ == "__main__":
     bot.run(TOKEN)
